9ebd00d22f7f8a912fbaf3f97321f7ad76adbee1
cospan <cospan@gmail.com>
2014-11-04 16:13:30 -0500

    Incorporating seeed_tft

--- verilog/wishbone/slave/wb_seeed_tft/rtl/seeed_tft_defines.v
+++ verilog/wishbone/slave/wb_seeed_tft/rtl/seeed_tft_defines.v
@@ -1,23 +1,20 @@
-`ifndef __NH_LCD_DEFINES__
-`define __NH_LCD_DEFINES__
+`ifndef __SEEED_TFT_DEFINES__
+`define __SEEED_TFT_DEFINES__
 
 `timescale 1 ns/1 ps
 //Initially 2MB data chunk
 `define DEFAULT_MEM_0_BASE        32'h00000000
 //`ifndef SIMULATION
-`define DEFAULT_MEM_1_BASE        32'h00100000
+`define DEFAULT_MEM_1_BASE        32'h00200000
 //`else
 //  `define DEFAULT_MEM_1_BASE        32'h00000008
 //`endif
-
-
+`define DELAY_COUNT                 8'h02
 
 
 //Command Registers
-`define CMD_START_MEM_WRITE 8'h2C
+`define CMD_START_MEM_WRITE 8'h22
 `define CMD_GET_TEAR_STATUS 8'h0E
-
-
 
 `endif
 
