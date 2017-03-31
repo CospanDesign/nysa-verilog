ca9da28f3bde022bf80b7a34a4e55e216f716be9
cospan <cospan@gmail.com>
2017-03-31 09:48:15 -0400

    Updated copyright date

--- verilog/generic/template.v
+++ verilog/generic/template.v
@@ -1,6 +1,6 @@
 /*
 Distributed under the MIT license.
-Copyright (c) 2016 Dave McCoy (dave.mccoy@cospandesign.com)
+Copyright (c) 2017 Dave McCoy (dave.mccoy@cospandesign.com)
 
 Permission is hereby granted, free of charge, to any person obtaining a copy of
 this software and associated documentation files (the "Software"), to deal in
