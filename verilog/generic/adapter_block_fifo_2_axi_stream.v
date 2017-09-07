16b5972d0cb04b02303641753e37be33ad5fbc96
cospan <cospan@gmail.com>
2017-09-07 14:01:17 -0400

    changed the names to be consistant

--- verilog/generic/adapter_block_fifo_2_axi_stream.v
+++ verilog/generic/adapter_block_fifo_2_axi_stream.v
@@ -35,7 +35,7 @@ SOFTWARE.
 
 `timescale 1ps / 1ps
 
-module adapter_bfifo_2_axi_stream #(
+module adapter_block_fifo_2_axi_stream #(
   parameter                                     DATA_WIDTH          = 24,
   parameter                                     STROBE_WIDTH        = DATA_WIDTH / 8,
   parameter                                     USE_KEEP            = 0
@@ -43,11 +43,11 @@ module adapter_bfifo_2_axi_stream #(
   input                                         rst,
 
   //Ping Poing FIFO Read Interface
-  input                                         i_bfifo_rdy,
-  output  reg                                   o_bfifo_act,
-  input       [23:0]                            i_bfifo_size,
-  input       [(DATA_WIDTH + 1) - 1:0]          i_bfifo_data,
-  output                                        o_bfifo_stb,
+  input                                         i_block_fifo_rdy,
+  output  reg                                   o_block_fifo_act,
+  input       [23:0]                            i_block_fifo_size,
+  input       [(DATA_WIDTH + 1) - 1:0]          i_block_fifo_data,
+  output                                        o_block_fifo_stb,
 
   //AXI Stream Output
 
@@ -72,22 +72,22 @@ reg     [23:0]              r_count;
 
 //submodules
 //asynchronous logic
-assign  o_axi_data      = i_bfifo_data[DATA_WIDTH - 1: 0];
-assign  o_bfifo_stb    = (i_axi_ready & o_axi_valid);
+assign  o_axi_data      = i_block_fifo_data[DATA_WIDTH - 1: 0];
+assign  o_block_fifo_stb= (i_axi_ready & o_axi_valid);
 
-assign  o_axi_user[0]   = (r_count < i_bfifo_size) ? i_bfifo_data[DATA_WIDTH] : 1'b0;
+assign  o_axi_user[0]   = (r_count < i_block_fifo_size) ? i_block_fifo_data[DATA_WIDTH] : 1'b0;
 assign  o_axi_user[3:1] = 3'h0;
 
-assign  o_axi_last      = ((r_count + 1) >= i_bfifo_size) & o_bfifo_act  & o_axi_valid;
+assign  o_axi_last      = ((r_count + 1) >= i_block_fifo_size) & o_block_fifo_act  & o_axi_valid;
 //synchronous logic
 
 assign  o_debug[3:0]    = state;
-assign  o_debug[4]      = (r_count < i_bfifo_size) ? i_bfifo_data[DATA_WIDTH]: 1'b0;
-assign  o_debug[5]      = o_bfifo_act;
-assign  o_debug[6]      = i_bfifo_rdy;
+assign  o_debug[4]      = (r_count < i_block_fifo_size) ? i_block_fifo_data[DATA_WIDTH]: 1'b0;
+assign  o_debug[5]      = o_block_fifo_act;
+assign  o_debug[6]      = i_block_fifo_rdy;
 assign  o_debug[7]      = (r_count > 0);
-assign  o_debug[8]      = (i_bfifo_size > 0);
-assign  o_debug[9]      = (r_count == i_bfifo_size);
+assign  o_debug[8]      = (i_block_fifo_size > 0);
+assign  o_debug[9]      = (r_count == i_block_fifo_size);
 assign  o_debug[15:10]  = 0;
 assign  o_debug[23:16]  = r_count[7:0];
 assign  o_debug[31:24]  = 0;
@@ -97,31 +97,31 @@ always @ (posedge i_axi_clk) begin
 
   if (rst) begin
     state               <=  IDLE;
-    o_bfifo_act        <=  0;
+    o_block_fifo_act    <=  0;
     r_count             <=  0;
   end
   else begin
     case (state)
       IDLE: begin
-        o_bfifo_act    <=  0;
-        if (i_bfifo_rdy && !o_bfifo_act) begin
+        o_block_fifo_act    <=  0;
+        if (i_block_fifo_rdy && !o_block_fifo_act) begin
           r_count       <=  0;
-          o_bfifo_act  <=  1;
+          o_block_fifo_act  <=  1;
           state         <=  READY;
         end
       end
       READY: begin
-        if (r_count < i_bfifo_size) begin
+        if (r_count < i_block_fifo_size) begin
           o_axi_valid         <=  1;
           if (i_axi_ready && o_axi_valid) begin
             r_count         <= r_count + 1;
-            if ((r_count + 1) >= i_bfifo_size) begin
+            if ((r_count + 1) >= i_block_fifo_size) begin
               o_axi_valid     <=  0;
             end
           end
         end
         else begin
-          o_bfifo_act       <=  0;
+          o_block_fifo_act  <=  0;
           state             <=  RELEASE;
         end
       end
